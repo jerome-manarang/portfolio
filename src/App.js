4fae81c5c1d6e13a26d764f41824ca3ed2c2e806
Jerome Manarang <jemanara@uci.edu>
2025-02-13 20:17:58 -0800

    included experience section with cloud

--- src/App.js
+++ src/App.js
@@ -54,6 +54,7 @@ function App() {
       <StickyHeader />
       <Main />
       <Projects />
+      <Experience />
       
       
     </div>
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -40,7 +40,7 @@ function Experience() {
             data-aos-delay={`${index * 200}`}
           >
             <a
-              href={project.githubLink}
+              href={project.Link}
               target="_blank"
               rel="noopener noreferrer"
               className="cloud-project"
@@ -48,11 +48,11 @@ function Experience() {
               <img
                 src={cloudImage}
                 alt="cloud"
-                className="cloud-image img-fluid"
+                className="cloud-image-exp img-fluid"
               />
               <div className="cloud-content">
-                <h3>{experience.title}</h3>
-                <p>{experience.description}</p>
+                <h3>{project.title}</h3>
+                <p>{project.description}</p>
               </div>
             </a>
           </div>
