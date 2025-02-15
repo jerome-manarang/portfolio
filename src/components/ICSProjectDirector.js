26b654d8030a51b9c58afd1166e6163ca5a44bf4
Jerome Manarang <jemanara@uci.edu>
2025-02-14 23:49:55 -0800

    added images and information to new ics page

--- src/components/ICSProjectDirector.js
+++ src/components/ICSProjectDirector.js
@@ -1,6 +1,8 @@
 import React from "react";
 import '../ICSProjectDirector.css';
 import { motion } from "framer-motion";
+import fipcrew from '../images/FipCrew.JPG';
+import fipteams from '../images/FIPTeams.jpg';
 
 function ICSProjectDirector() {
   return (
@@ -11,7 +13,17 @@ function ICSProjectDirector() {
       transition={{ duration: 1.5, ease: "easeOut" }} // Smooth transition
     >
       <h1>Information and Computer Science Project Director</h1>
+      <p>As the project director, Jerome Manarang lead five teams in their project development.</p>
+      
+      <div className="image-container">
+        <img src={fipcrew} alt="fip" className="fip-image-one"/>
+        <img src={fipteams} alt="fipteams" className="fip-image-two"/>
+      </div>
+ 
     </motion.div>
+
+
+
   );
 }
 
